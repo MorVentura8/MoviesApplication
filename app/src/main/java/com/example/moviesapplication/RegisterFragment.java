d941714bc17d5330e23f544ae594490fb3b7033e
Segevc98 <156806005+Segevc98@users.noreply.github.com>
2025-02-22 16:17:25 +0200

    Fixed error in RegisterFragment

--- app/src/main/java/com/example/moviesapplication/RegisterFragment.java
+++ app/src/main/java/com/example/moviesapplication/RegisterFragment.java
@@ -1,19 +1,22 @@
 package com.example.moviesapplication;
 
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
-
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
+import com.example.moviesapplication.data.model.Users;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
